4b0864d9668bd20b6cbb0e3eb827022e1dcff1be
zk_mac_test <519715203@qq.com>
2019-03-19 17:21:13 +0800

    add new env

---
+++ env.py
@@ -0,0 +1,21 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ==============================================================================
+#   Copyright (C) 2019 MyCompany. All rights reserved.
+#   
+#   File Name  : env.py
+#   Author     : Zhaokui
+#   Create Date: 2019-03-19 17:20
+#   Describle  : 
+#
+# ==============================================================================
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+

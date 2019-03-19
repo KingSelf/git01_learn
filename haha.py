9960d91985428826c5821dbaf7dcf3e851dbee9f
zk_test <519715203@qq.com>
2019-03-19 16:16:09 +0800

    haha.py

---
+++ haha.py
@@ -0,0 +1,21 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ==============================================================================
+#   Copyright (C) 2019 MyCompany. All rights reserved.
+#   
+#   File Name  : haha.py
+#   Author     : Zhaokui
+#   Create Date: 2019-03-19 16:13
+#   Describle  : 
+#
+# ==============================================================================
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
